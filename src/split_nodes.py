2414515d7147b7c11caf138628dee7b3674c1fb1
Shaun Chowthee <sn.chowthee@gmail.com>
2026-01-30 01:57:11 +0100

    feat: added additionnal splitting nodes functions + tests

--- src/split_nodes.py
+++ src/split_nodes.py
@@ -1,4 +1,5 @@
 from src.textnode import TextType, TextNode
+from src.markdown_links_extraction import extract_markdown_images, extract_markdown_links
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     new_nodes = []
@@ -18,4 +19,62 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
                     else:
                         new_node = TextNode(part, text_type)
                     new_nodes.append(new_node)
+    return new_nodes
+
+def split_nodes_image(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+            continue
+        
+        text = node.text
+        matches = extract_markdown_images(text)
+
+        if not matches:
+            new_nodes.append(node)
+            continue
+
+        for alt, url in matches:
+            markdown = f"![{alt}]({url})"
+            parts = text.split(markdown, 1)
+            before = parts[0]
+            after = parts[1]
+            if before != "":
+                new_nodes.append(TextNode(before, TextType.TEXT))
+            new_nodes.append(TextNode(alt, TextType.IMAGE, url))
+            text = after
+        if text != "":
+            new_nodes.append(TextNode(text, TextType.TEXT))
+    return new_nodes
+
+
+
+            
+
+def split_nodes_link(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+            continue
+        
+        text = node.text
+        matches = extract_markdown_links(text)
+
+        if not matches:
+            new_nodes.append(node)
+            continue
+
+        for anchor, url in matches:
+            markdown = f"[{anchor}]({url})"
+            parts = text.split(markdown, 1)
+            before = parts[0]
+            after = parts[1]
+            if before != "":
+                new_nodes.append(TextNode(before, TextType.TEXT))
+            new_nodes.append(TextNode(anchor, TextType.LINK, url))
+            text = after
+        if text != "":
+            new_nodes.append(TextNode(text, TextType.TEXT))
     return new_nodes
--- src/tests/test_split_nodes.py
+++ src/tests/test_split_nodes.py
@@ -1,5 +1,5 @@
 import unittest
-from src.split_nodes import split_nodes_delimiter
+from src.split_nodes import split_nodes_delimiter, split_nodes_image, split_nodes_link
 from src.textnode import TextNode, TextType
 
 class TestSplitNodesFct(unittest.TestCase):
@@ -32,3 +32,107 @@ class TestSplitNodesFct(unittest.TestCase):
         node = TextNode(txt, TextType.TEXT)
         with self.assertRaises(Exception):
             split_nodes_delimiter([node], "**", TextType.BOLD)
+    
+    def test_non_text_nodes_unchanged(self):
+        nodes = [TextNode("hello", TextType.BOLD)]
+        self.assertEqual(split_nodes_image(nodes), nodes)
+
+    def test_no_images_returns_original_text_node(self):
+        node = TextNode("just plain text", TextType.TEXT)
+        self.assertEqual(split_nodes_image([node]), [node])
+
+    def test_single_image_middle(self):
+        node = TextNode("hi ![cat](https://x.com/cat.png) bye", TextType.TEXT)
+        self.assertEqual(
+            split_nodes_image([node]),
+            [
+                TextNode("hi ", TextType.TEXT),
+                TextNode("cat", TextType.IMAGE, "https://x.com/cat.png"),
+                TextNode(" bye", TextType.TEXT),
+            ],
+        )
+
+    def test_two_images(self):
+        node = TextNode(
+            "A ![one](u1) B ![two](u2) C",
+            TextType.TEXT,
+        )
+        self.assertEqual(
+            split_nodes_image([node]),
+            [
+                TextNode("A ", TextType.TEXT),
+                TextNode("one", TextType.IMAGE, "u1"),
+                TextNode(" B ", TextType.TEXT),
+                TextNode("two", TextType.IMAGE, "u2"),
+                TextNode(" C", TextType.TEXT),
+            ],
+        )
+
+    def test_image_at_start_and_end(self):
+        node = TextNode("![a](u1) mid ![b](u2)", TextType.TEXT)
+        self.assertEqual(
+            split_nodes_image([node]),
+            [
+                TextNode("a", TextType.IMAGE, "u1"),
+                TextNode(" mid ", TextType.TEXT),
+                TextNode("b", TextType.IMAGE, "u2"),
+            ],
+        )
+
+
+    def test_non_text_nodes_unchanged(self):
+        nodes = [TextNode("hello", TextType.CODE)]
+        self.assertEqual(split_nodes_link(nodes), nodes)
+
+    def test_no_links_returns_original_text_node(self):
+        node = TextNode("just plain text", TextType.TEXT)
+        self.assertEqual(split_nodes_link([node]), [node])
+
+    def test_single_link_middle(self):
+        node = TextNode("hi [boot](https://boot.dev) bye", TextType.TEXT)
+        self.assertEqual(
+            split_nodes_link([node]),
+            [
+                TextNode("hi ", TextType.TEXT),
+                TextNode("boot", TextType.LINK, "https://boot.dev"),
+                TextNode(" bye", TextType.TEXT),
+            ],
+        )
+
+    def test_two_links(self):
+        node = TextNode("A [one](u1) B [two](u2) C", TextType.TEXT)
+        self.assertEqual(
+            split_nodes_link([node]),
+            [
+                TextNode("A ", TextType.TEXT),
+                TextNode("one", TextType.LINK, "u1"),
+                TextNode(" B ", TextType.TEXT),
+                TextNode("two", TextType.LINK, "u2"),
+                TextNode(" C", TextType.TEXT),
+            ],
+        )
+
+    def test_link_at_start_and_end(self):
+        node = TextNode("[a](u1) mid [b](u2)", TextType.TEXT)
+        self.assertEqual(
+            split_nodes_link([node]),
+            [
+                TextNode("a", TextType.LINK, "u1"),
+                TextNode(" mid ", TextType.TEXT),
+                TextNode("b", TextType.LINK, "u2"),
+            ],
+        )
+
+    def test_link_split_does_not_touch_images(self):
+        node = TextNode("x ![img](img-url) y [lnk](lnk-url) z", TextType.TEXT)
+        self.assertEqual(
+            split_nodes_link([node]),
+            [
+                TextNode("x ![img](img-url) y ", TextType.TEXT),
+                TextNode("lnk", TextType.LINK, "lnk-url"),
+                TextNode(" z", TextType.TEXT),
+            ],
+        )
+
+if __name__ == "__main__":
+    unittest.main()
