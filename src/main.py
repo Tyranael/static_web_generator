361da6b78c6244854b6401715b0a39232f44974c
Shaun Chowthee <sn.chowthee@gmail.com>
2026-02-03 01:16:01 +0100

    feat: added the generator, copying from static to public

--- src/main.py
+++ src/main.py
@@ -1,6 +1,23 @@
-from src.textnode import TextNode, TextType
-def main():
-    new_tn = TextNode("hi", TextType("bold"), "cool.com")
-    print(new_tn)
+import os
+import shutil
 
-main()
+
+def copy_static(src, dest):
+    if os.path.exists(dest):
+        shutil.rmtree(dest)
+    os.mkdir(dest)
+
+    # 2) walk source
+    for entry in os.listdir(src):
+        src_path = os.path.join(src, entry)
+        dest_path = os.path.join(dest, entry)
+
+        if os.path.isfile(src_path):
+            print(f"Copying {src_path} -> {dest_path}")
+            shutil.copy(src_path, dest_path)
+        else:
+            # directory
+            copy_static(src_path, dest_path)
+
+
+copy_static("static", "public")
