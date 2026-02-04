ac003bedcb12fe33ef67bb7a371c864414eac014
Shaun Chowthee <sn.chowthee@gmail.com>
2026-02-04 01:37:19 +0100

    feat: added main files/fct

--- src/main.py
+++ src/main.py
@@ -1,25 +1,29 @@
 import os
 import shutil
-
+from src.page_generator import generate_page
 
 def copy_static(src, dest):
-    if not os.path.exists(src):
-        raise Exception("must put valid source directory")
-    if os.path.exists(dest):
-        shutil.rmtree(dest)
-    os.mkdir(dest)
+        if not os.path.exists(src):
+            raise Exception("must put valid source directory")
+        if os.path.exists(dest):
+            shutil.rmtree(dest)
+        os.mkdir(dest)
 
-    # 2) walk source
-    for entry in os.listdir(src):
-        src_path = os.path.join(src, entry)
-        dest_path = os.path.join(dest, entry)
+        for entry in os.listdir(src):
+            src_path = os.path.join(src, entry)
+            dest_path = os.path.join(dest, entry)
 
-        if os.path.isfile(src_path):
-            print(f"Copying {src_path} -> {dest_path}")
-            shutil.copy(src_path, dest_path)
-        else:
-            # directory
-            copy_static(src_path, dest_path)
+            if os.path.isfile(src_path):
+                print(f"Copying {src_path} -> {dest_path}")
+                shutil.copy(src_path, dest_path)
+            else:
+                copy_static(src_path, dest_path)
 
 
-copy_static("static", "public")
+
+def main():
+    copy_static("static", "public")
+    generate_page("content/index.md", "template.html", "public/index.html")
+
+
+main()
